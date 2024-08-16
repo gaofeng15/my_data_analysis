2bbc3e1e2f05da58c66a4f70da7c4cfb2af5876f
gaofeng15 <278331633@qq.com>
2024-08-16 21:27:59 +0800

    PRACTICE.py

--- PRACTICE.py
+++ PRACTICE.py
@@ -13,18 +13,18 @@
 #     doc.save(f'./结果/{xingming}.docx')
 
 
-# import streamlit as st,pandas as pd
-#
-# def zongfen(df):
-#     df1 = df.groupby('班级').agg(**{'总分': ('成绩', 'sum')})
-#     return df1
-# upload_file = st.file_uploader(label='上传要处理的文件')
-# butt = st.button('显示各班总分')
-# if upload_file:
-#
-#     df = pd.read_excel(upload_file)
-# if butt:
-#     st.write(zongfen(df))
+import streamlit as st,pandas as pd
+
+def zongfen(df):
+    df1 = df.groupby('班级').agg(**{'总分': ('成绩', 'sum')})
+    return df1
+upload_file = st.file_uploader(label='上传要处理的文件')
+butt = st.button('显示各班总分')
+if upload_file:
+
+    df = pd.read_excel(upload_file)
+if butt:
+    st.write(zongfen(df))
 
 import pandas as pd
 import streamlit as st
